1bc89008eef44f2409868267dcd2bd2ec1de78be
nghiapp7018 <nghia.pham@co-well.vn>
2020-05-11 15:16:40 +0700

    edit uuid package

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var uid = require('uid');
+const { v4: uuidv4 } = require('uuid');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "express", uid: uid()});
+  res.render('index', { title: "express", uid: uuidv4()});
 });
 
 module.exports = router;
