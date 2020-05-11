f34e8bd25452bc5e1baa418ab3da80dcf0b9c612
nghiapp7018 <nghia.pham@co-well.vn>
2020-05-11 15:37:06 +0700

    add moment

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 var uid = require('uid');
 const { v4: uuidv4 } = require('uuid');
+const moment = require('moment');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Express", uid: uuidv4()});
+  res.render('index', { title: "Express", uid: uuidv4(), now: moment().format('MMMM Do YYYY, h:mm:ss a')});
 });
 
 module.exports = router;
